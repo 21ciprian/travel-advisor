67646ffcb3492be9849a249f31d2a4bab991426d
21ciprian <2021ciprianoffice@gmail.com>
2022-04-02 16:31:05 +0100

    Added Map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,10 @@
+import styles from 'Map.module.css'
+import React from 'react'
+
+type Props = {}
+
+function Map({}: Props) {
+	return <div className={styles.map}>Map</div>
+}
+
+export default Map
